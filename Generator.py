9d4978221e9139ecb68a56af64b9800b9d5cba40
adroitous <32344162+adroitous@users.noreply.github.com>
2017-12-14 11:33:16 -0500

    Fixed bug with generation

--- Generator.py
+++ Generator.py
@@ -3,14 +3,13 @@ import json
 with open('category_dict.json') as json_data:
     category_dict = json.load(json_data)
 
-
 with open('video_dict.json') as json_data:
     video_dict = json.load(json_data)
 
 def category_generator(name):
     category_corpus = []
-    for i in range(len(category_dict)):  
-       	video = category_dict[name][i][0]
+    for i in range(len(category_dict[name])):  
+       	video = category_dict[name][i]
         try:
             category_corpus.append(video_dict[video])
         except KeyError:
@@ -29,5 +28,4 @@ def corpus_generation(name):
     with open('category_corpus.txt', 'w') as f:
         for e in corpus: 
             f.write(str(e) +'\n')
-
 
