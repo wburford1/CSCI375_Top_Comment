921c8d297869a4ba29639e6852957c4c4bf15779
Haoyu Sheng <haoyusheng@Haoyus-MacBook-Pro.local>
2017-12-15 14:57:02 -0500

    top comment generator

--- top_comment.py
+++ top_comment.py
@@ -1,9 +1,22 @@
 # requires the video_id to be read in as first command line arg
 from controller import ensemble_run, parse_video_dict
 import sys
+from random import sample
 import json
 
+def top(category_id, video_dict, top_comments):
+    with open('category_dict.json', 'r') as f:
+        category_dict = json.load(f)
+    videos = [video_info[0] for video_info in category_dict[category_id]]
+    videos = [videos[i] for i in sample(range(len(videos)), 4)]
+    top_comments = [c.strip() for c in top_comments if not c.isspace() and c != ""]
+    video_dict = parse_video_dict()
+    #best = ensemble_run(video_dict, top_comments, video_id)
+    #return ('Best comment is: {}'.format(best))
+    return 0
+
 if __name__ == '__main__':
+    '''
     video_id = sys.argv[1]
     with open('comment_generated.json', 'r') as f:
         coms = json.load(f)
@@ -14,3 +27,6 @@ if __name__ == '__main__':
         video_dict = parse_video_dict()
         best = ensemble_run(video_dict, coms, video_id)
         print('Best comment is: {}'.format(best))
+        '''
+    video_dict = parse_video_dict()
+    top("24", video_dict, ['awdawboir', 'dwaoirhiow'])
